d053ac069d1989a19f862a0ca4b5072d214d56f8
DanielDiaz18 <Dani.diaz.g.32@gmail.com>
2019-02-16 10:36:45 -0600

    Daniel Diaz

--- main.c
+++ main.c
@@ -16,8 +16,24 @@ int omarAlvizo()
 	return r;
 }
 
+int danielDiaz()
+{
+
+	char* nombre = "Daniel Diaz";
+	int len = strlen(nombre);
+	int i, r = 0;
+
+	for(i = 0; i < len;i++)
+	{
+		r += nombre[i];
+	}
+
+	return r;
+}
+
 int main()
 {
-	printf("Mi valor en ASCCI es %i", omarAlvizo());
+	printf("Mi valor en ASCCI es %i\n", omarAlvizo());
+	printf("Mi valor en ASCCI es %i\n", danielDiaz());
 	return 0;
 }
