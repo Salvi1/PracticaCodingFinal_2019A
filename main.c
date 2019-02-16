4d3f803945188ff704ecc499f7f29d1521bfa423
diegoivan1987 <diegoivan1987@outlook.com>
2019-02-16 10:34:29 -0600

    mi nombre

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<string.h>
-
 int omarAlvizo()
 {
 
@@ -16,8 +15,24 @@ int omarAlvizo()
 	return r;
 }
 
+int diegoBecerra()
+{
+
+	char* nombre1 = "Diego Becerra";
+	int len1 = strlen(nombre1);
+	int i1, r1 = 0;
+
+	for(i1 = 0; i1 < len1;i1++)
+	{
+		r1 += nombre1[i1];
+	}
+
+	return r1;
+}
+
 int main()
 {
 	printf("Mi valor en ASCCI es %i", omarAlvizo());
+	printf("\nMi valor en ASCCI es %i", diegoBecerra());
 	return 0;
 }
