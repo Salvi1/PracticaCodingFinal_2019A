a616f02b32d2ba0923212c681559d5066fc98343
Salvi1 <narut16@hotmail.com>
2019-02-16 10:43:25 -0600

    Mi nombre juas juas

--- main.c
+++ main.c
@@ -16,8 +16,24 @@ int omarAlvizo()
 	return r;
 }
 
+int juanSalazar()
+{
+
+	char* nombre_j = "Juan Salazar";
+	int len_j = strlen(nombre_j);
+	int i_j, r_j = 0;
+
+	for(i_j = 0; i_j < len_j;i_j++)
+	{
+		r_j += nombre_j[i_j];
+	}
+
+	return r_j;
+}
+
 int main()
 {
 	printf("Mi valor en ASCCI es %i", omarAlvizo());
+	printf("\nMi valor en ASCCI es %i",juanSalazar());
 	return 0;
 }
