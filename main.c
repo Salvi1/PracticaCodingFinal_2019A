6bfc4b8816a5ad4c1c272c6262c24b6d387c11d1
Theyayoman <jair.orozco2000@gmail.com>
2019-02-16 10:34:09 -0600

    Yayazo estuvo aqui

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 
 int omarAlvizo()
 {
-
 	char* nombre = "Omar Alvizo";
 	int len = strlen(nombre);
 	int i, r = 0;
@@ -16,8 +15,26 @@ int omarAlvizo()
 	return r;
 }
 
+int jairOrozco()
+{
+	char* nombre = "Jair Orozco";
+	int len = strlen(nombre);
+	int i, r = 0;
+
+	for(i = 0; i < len;i++)
+	{
+		r += nombre[i];
+	}
+
+	return r;
+
+}
+
+
 int main()
 {
-	printf("Mi valor en ASCCI es %i", omarAlvizo());
+	printf("Mi valor en ASCCI es %i \n", omarAlvizo());
+	printf("Mi valor es ASCCI es %i \n", jairOrozco());
 	return 0;
+
 }
