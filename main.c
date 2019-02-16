e6b75acffc529e7bdd59b2af0b8a75e7525c91aa
000225 <000225>
2019-02-16 10:28:57 -0600

    Ezequiel De la Torre

--- main.c
+++ main.c
@@ -16,8 +16,25 @@ int omarAlvizo()
 	return r;
 }
 
+int EzequielDelaTorre()
+{
+
+	char* nombre = "Ezequiel De la Torre";
+	int len = strlen(nombre);
+	int i, r = 0;
+
+	for(i = 0; i < len;i++)
+	{
+		r += nombre[i];
+	}
+
+	return r;
+}
+
+
 int main()
 {
-	printf("Mi valor en ASCCI es %i", omarAlvizo());
+	printf("Mi valor en ASCCI es %i\n", omarAlvizo());
+	printf("Mi valor en ASCCI es %i", EzequielDelaTorre());
 	return 0;
 }
