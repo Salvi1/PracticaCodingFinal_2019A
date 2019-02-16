e35d1fe785e4e5b99b8cd39c4cbbb323282a47ff
lesliejocelyn6 <lmm_005-leslie@hotmail.com>
2019-02-16 10:29:29 -0600

    Agregue la funcion leslieMorales

--- main.c
+++ main.c
@@ -16,8 +16,25 @@ int omarAlvizo()
 	return r;
 }
 
+int leslieMorales()
+{
+
+	char* nombre = "Leslie Morales";
+	int len = strlen(nombre);
+	int i, r = 0;
+
+	for(i = 0; i < len;i++)
+	{
+		r += nombre[i];
+	}
+
+	return r;
+}
+
 int main()
 {
 	printf("Mi valor en ASCCI es %i", omarAlvizo());
+	printf("\nMi valor en ASCCI es %i\n", leslieMorales());
+	
 	return 0;
 }
