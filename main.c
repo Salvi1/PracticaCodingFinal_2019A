23a74a0be7ded75d01ce82d3ee829f317a7dabdb
julieta.renteria15@gmail.com <taoiepint@outlook.com>
2019-02-16 10:46:22 -0600

    daniel martinez

--- main.c
+++ main.c
@@ -16,8 +16,24 @@ int omarAlvizo()
 	return r;
 }
 
+int danielMartinez()
+{
+
+	char* nombre = "Daniel Martinez";
+	int len = strlen(nombre);
+	int i, r = 0;
+
+	for(i = 0; i < len;i++)
+	{
+		r += nombre[i];
+	}
+
+	return r;
+}
+
 int main()
 {
-	printf("Mi valor en ASCCI es %i", omarAlvizo());
+	printf("Mi valor en ASCCI es %i\n", omarAlvizo());
+	printf("Mi valor en ASCCI es %i\n", danielMartinez());
 	return 0;
 }
