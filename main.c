a95e6226808574122347d1c24754d852bd769007
Jose Ivan Orozco Torrez <vandel_van@hotmail.com>
2019-02-16 10:21:12 -0600

    añadi mi funcion

--- main.c
+++ main.c
@@ -16,8 +16,23 @@ int omarAlvizo()
 	return r;
 }
 
+int ivanOrozco(){
+
+	char* nombre = "Ivan Orozco";
+	int len = strlen(nombre);
+	int i, r = 0;
+
+	for(i = 0; i < len;i++)
+	{
+		r += nombre[i];
+	}
+
+	return r;
+}
+
 int main()
 {
 	printf("Mi valor en ASCCI es %i", omarAlvizo());
+	printf("Mi valor en ASCCI es %i", ivanOrozco());
 	return 0;
 }
