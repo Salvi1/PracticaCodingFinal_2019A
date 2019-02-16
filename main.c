b06d5059ad97cc29bfef9edd94975381a87c543c
Sujey Galvan <sujeygalvan03@gmail.com>
2019-02-16 10:42:20 -0600

    jose manuel alvarez gonzalez

--- main.c
+++ main.c
@@ -16,8 +16,24 @@ int omarAlvizo()
 	return r;
 }
 
+int joseAlvarez()
+{
+
+	char* nombre = "Jose Alvarez";
+	int len = strlen(nombre);
+	int i, r = 0;
+
+	for(i = 0; i < len;i++)
+	{
+		r += nombre[i];
+	}
+
+	return r;
+}
+
 int main()
 {
-	printf("Mi valor en ASCCI es %i", omarAlvizo());
+	printf("Mi valor en ASCCI es %i\n", omarAlvizo());
+	printf("Mi valor en ASCCI es %i\n", joseAlvarez());
 	return 0;
 }
