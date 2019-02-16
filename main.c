27f295ae7b32a3cd74d29af5ee45042afda1303a
Javier10Ramirez <jw.org10@hotmail.com>
2019-02-16 10:38:33 -0600

    Messi

--- main.c
+++ main.c
@@ -16,8 +16,24 @@ int omarAlvizo()
 	return r;
 }
 
+int javierRamirez()
+{
+
+	char* nombre = "Javier Ramirez";
+	int len = strlen(nombre);
+	int i, r = 0;
+
+	for(i = 0; i < len;i++)
+	{
+		r += nombre[i];
+	}
+
+	return r;
+}
+
 int main()
 {
 	printf("Mi valor en ASCCI es %i", omarAlvizo());
+	printf("Mi valor en ASCCI es %i", javierRamirez());
 	return 0;
 }
