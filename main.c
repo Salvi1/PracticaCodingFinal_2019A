47aeda66599070befac040462ba077aa56708a1f
PedroLopezSi <pedro.lopez010900@gmail.com>
2019-02-16 10:31:19 -0600

    Soy peter ya quedo

--- main.c
+++ main.c
@@ -16,8 +16,24 @@ int omarAlvizo()
 	return r;
 }
 
+int pedroLopez()
+{
+
+	char* nombre = "Pedro Lopez";
+	int len = strlen(nombre);
+	int i, r = 0;
+
+	for(i = 0; i < len;i++)
+	{
+		r += nombre[i];
+	}
+
+	return r;
+}
+
 int main()
 {
-	printf("Mi valor en ASCCI es %i", omarAlvizo());
+	printf("Mi valor en ASCCI es %i \n ", omarAlvizo());
+	printf("Soy pedro y Mi valor en ASCCI es %i \n ", pedroLopez());
 	return 0;
 }
