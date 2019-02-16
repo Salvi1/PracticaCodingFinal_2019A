d30b95fbcba43db027c8c4c85eb4a4e8187fecde
unknown <esme_503@outlook.com>
2019-02-16 10:29:02 -0600

    Agregue la funcion anaGonzalez

--- main.c
+++ main.c
@@ -16,8 +16,24 @@ int omarAlvizo()
 	return r;
 }
 
+int anaGonzalez()
+{
+
+	char* nombre = "Ana Gonzalez";
+	int len = strlen(nombre);
+	int i, r = 0;
+
+	for(i = 0; i < len;i++)
+	{
+		r += nombre[i];
+	}
+
+	return r;
+}
+
 int main()
 {
 	printf("Mi valor en ASCCI es %i", omarAlvizo());
+	printf("Mi valor en ASCCI es %i", anaGonzalez());
 	return 0;
 }
