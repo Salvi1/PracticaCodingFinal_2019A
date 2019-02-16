45650f5f49aa723bef72713601cecb1db25b6911
AlexRochaGallegos <rochaalejandro2105@gmail.com>
2019-02-16 10:43:44 -0600

    Jose Alejandro Rocha

--- main.c
+++ main.c
@@ -16,8 +16,26 @@ int omarAlvizo()
 	return r;
 }
 
+
+int alexRocha()
+{
+
+	char* nombre = "Alex Rocha";
+	int len = strlen(nombre);
+	int i, r = 0;
+
+	for(i = 0; i < len;i++)
+	{
+		r += nombre[i];
+	}
+
+	return r;
+}
+
+
 int main()
 {
-	printf("Mi valor en ASCCI es %i", omarAlvizo());
+	printf("Mi valor en ASCCI es %i\n", omarAlvizo());
+	printf("Mi valor en ASCCI es %i\n", alexRocha());
 	return 0;
 }
