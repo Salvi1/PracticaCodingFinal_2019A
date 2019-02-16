5873a46270500e85b81fc0eecafaba757f411725
Axel Cruz <holaaxel01098@outlook.com>
2019-02-16 10:29:00 -0600

    Axel Cruz

--- main.c
+++ main.c
@@ -16,8 +16,24 @@ int omarAlvizo()
 	return r;
 }
 
+int axelCruz()
+{
+
+	char* nombre = "Axel Cruz";
+	int len = strlen(nombre);
+	int i, r = 0;
+
+	for(i = 0; i < len;i++)
+	{
+		r += nombre[i];
+	}
+
+	return r;
+}
+
 int main()
 {
-	printf("Mi valor en ASCCI es %i", omarAlvizo());
+	printf("Mi valor en ASCCI es %i\n", omarAlvizo());
+	printf("Mi valor en ASCCI es %i\n", axelCruz());
 	return 0;
 }
