a019b3d72c87daf20cf5d8e3ced2214cb657192f
David González <daleono@hotmail.com>
2019-02-16 10:36:31 -0600

    Mi parte

--- main.c
+++ main.c
@@ -16,8 +16,24 @@ int omarAlvizo()
 	return r;
 }
 
+int davidGonzalez()
+{
+
+	char* nombre = "David Gonzalez";
+	int len = strlen(nombre);
+	int i, r = 0;
+
+	for(i = 0; i < len;i++)
+	{
+		r += nombre[i];
+	}
+
+	return r;
+}
+
 int main()
 {
-	printf("Mi valor en ASCCI es %i", omarAlvizo());
+	printf("Mi valor en ASCCI es %i\n", omarAlvizo());
+	printf("Mi valor en ASCCI es %i\n", davidGonzalez());
 	return 0;
 }
