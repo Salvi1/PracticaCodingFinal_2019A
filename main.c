f5dc25729d3948dc0209f751394be8a6b0c239f7
Kevin Sanchez <sanchezkevin213329559@hotmail.com>
2019-02-16 10:35:56 -0600

    Agregue valor en ASCII de mi nombre

--- main.c
+++ main.c
@@ -16,8 +16,23 @@ int omarAlvizo()
 	return r;
 }
 
+int kevinSanchez()
+{
+
+	char* nombre = "Kevin Sanchez";
+	int len = strlen(nombre);
+	int i, r = 0;
+
+	for(i = 0; i < len;i++)
+	{
+		r += nombre[i];
+	}
+
+	return r;
+}
 int main()
 {
-	printf("Mi valor en ASCCI es %i", omarAlvizo());
+	printf("Mi valor en ASCCI es %i\n", omarAlvizo());
+	printf("Mi valor en ASCCI es %i\n", kevinSanchez());
 	return 0;
 }
